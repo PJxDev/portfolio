98013f7461390f201ff521cf6fd7620d0c78c7a9
Pedro <pjxp44@gmail.com>
2022-08-26 13:23:52 +0200

    [root] -- esqueleto creado

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import Header from './components/header/Header'
+import Nav from './components/nav/Nav'
+import About from './components/about/About'
+import Projects from './components/projects/Projects'
+import Tecnologies from './components/tecnologies/Tecnologies'
+import Contact from './components/contact/Contact'
+import Footer from './components/footer/Footer'
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Nav />
+      <About />
+      <Projects />
+      <Tecnologies />
+      <Contact />
+      <Footer />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/tecnologies/Tecnologies.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './tecnologies.css'
+
+const Tecnologies = () => {
+  return (
+    <section>Tecnologies</section>
+  )
+}
+
+export default Tecnologies
