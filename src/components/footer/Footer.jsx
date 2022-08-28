d3ddbad7e06bb3486c87818bd97e7679c88edfa3
PJxDev <pedrocruces@hotmail.es>
2022-08-28 23:10:36 +0200

    [FOOTER] -- minor changes on header and nav

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import './footer.css'
+import HeaderSocials from '../header/HeaderSocials'
 
 const Footer = () => {
   return (
     <footer>
-      
+      <div className="container footer__container">
+        <h4>Pedro J. Cruces - Desarrollador Web</h4>
+        <HeaderSocials />
+      </div>
     </footer>
   )
 }
