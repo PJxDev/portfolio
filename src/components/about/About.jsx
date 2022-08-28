9d7e6aadf15f957a7863da71fc70d93897f3ebd6
PJxDev <pedrocruces@hotmail.es>
2022-08-28 17:54:16 +0200

    [About] -- planteamiento

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
-import './about.css'
+import React from "react";
+import "./about.css";
+import Me2 from "../../assets/imgs/img2.jpg";
 
 const About = () => {
   return (
-    <section>About</section>
-  )
-}
+    <section>
+      <div className="container about__container">
+        <h1>Sobre mí</h1>
+        <div className="about__main">
+          <img src={Me2} alt="me computing" />
+          <div className="about__text">
+            <p>Nací en Málaga con dos pasiones: la música y la programación.</p>
+            <p>
+              Actualmente me encuentro inmerso en el CFGS de Desarrollo de
+              Aplicaciones Web, mientras que al mismo tiempo, me dedico a
+              investigar y a aprender nuevas tecnologías
+            </p>
+          </div>
+          <button></button>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default About
+export default About;
