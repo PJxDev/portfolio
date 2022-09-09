ba7f01b26b0e1a22189e15e61968bf598ff2518e
Pedro <pjxp44@gmail.com>
2022-09-09 13:11:47 +0200

    [BG] -- minor fix

--- src/components/services/BackgroundSpecial.jsx
+++ src/components/services/BackgroundSpecial.jsx
@@ -4,16 +4,6 @@ import { useSVGArray } from "../hooks/useSvgArray";
 
 const { svgArray: arraySVGs } = useSVGArray();
 
-console.log(arraySVGs[0]);
-
-const myobjArr = []
-const myobjArrFunc = () => {
-  for (let i = 0; i < arraySVGs.length; i++) {
-    myobjArr[i] = {svg:arraySVGs[i], isAnimating:false}
-  }
-};
-
-
 function randomNumber() {
   let random = () => Math.floor(Math.random() * arraySVGs.length);
   let numbers = [random(), random(), random(), random()];
@@ -32,15 +22,10 @@ const BackgroundSpecial = () => {
   const [number, setNumber] = useState([0, 1, 2, 3]);
   
   useEffect(() => {
-    
-    myobjArrFunc()
-    myobjArr[1].isAnimating = true
-    console.log(myobjArr[1]);
-
 
     const interval = setInterval(() => {
       setNumber(randomNumber());
-    }, 7000);
+    }, 10000);
     return () => clearInterval(interval);
   }, []);
 
